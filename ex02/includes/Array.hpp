c6f1eec0b50a076945a45e7b54c04f287b30028e
Mathis CATAL <catalmathis@gmail.com>
2023-06-26 15:21:46 +0200

    encours

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -6,30 +6,64 @@
 /*   By: mcatal-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 12:53:28 by mcatal-d          #+#    #+#             */
-/*   Updated: 2023/06/26 14:24:18 by mcatal-d         ###   ########.fr       */
+/*   Updated: 2023/06/26 15:21:34 by mcatal-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-#include <iostream>
 
 template <typename T>
 class Array
 {
     private:
         T* array;
-        size_t _size;
+        unsigned int _size;
     public:
-        Array();
-        Array(unsigned int n);
-        Array(T const &copy);
-        ~Array();
-        Array    &operator=(const T &copy);
-        T&  operator[](unsigned int n);
+        Array()
+        {
+            _size = 0;
+            array = NULL;
+        }
+        Array(unsigned int n)
+        {
+            array = new T [n];
+            _size = n;
+        }
+        Array(Array const &copy) : _size(copy._size)
+        {
+            array = new T [_size];
+            for(unsigned int i = 0; i < _size; i++)
+            {array[i] = copy.array[i];}
+        }
+        ~Array()
+        {
+          if (array)
+            delete [] array;
+        }
+        Array    &operator=(const T &copy)
+        {
+            if (this != copy)
+            {
+                ~Array();
+                _size = copy.size();
+                array = new T [_size];
+                for(unsigned int i = 0; i < _size; i++)
+                {array[i] = copy.array[i];}
+            }
+            return(this);
+        }
+        T&  operator[](unsigned int n)
+        {
+            if (n < _size)
+                return array[n];
+            throw InvalidIndexException();
+        }
+    
     public:
-        size_t size(void) const;
+        unsigned int size(void) const
+        {return _size;}
     class InvalidIndexException : public std::exception
     {
         public:
